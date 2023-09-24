e16961189d3d1cbeddca82cbdb14600dc93a19be
雨禾 赵 <xiaoyuhe103@outlook.com>
2023-09-24 17:50:17 +0800

    实现了类访问EXE文件
    添加了网站路径查找页以及一个测试运行程序

---
+++ EXE/EXEHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE
+{
+    public class EXEHelper
+    {
+        /// <summary>
+        /// exe文件绝对路径
+        /// </summary>
+        public string path;
+
+        public string arguments;
+        public string OperationReturn()
+        {
+            Process exe = new Process();
+            exe.StartInfo.FileName = path;
+            exe.StartInfo.Arguments = " "+arguments;
+            exe.StartInfo.UseShellExecute = false;
+            exe.StartInfo.RedirectStandardOutput = true;
+            exe.StartInfo.RedirectStandardInput = true;
+            exe.StartInfo.CreateNoWindow = true;
+            exe.Start();
+            exe.WaitForExit();
+            string data = exe.StandardOutput.ReadToEnd();
+            return data;
+        }
+    }
+}
---
+++ web/findTheWebpath.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EXE;
+
+public partial class test : System.Web.UI.Page
+{
+    string path = System.AppDomain.CurrentDomain.BaseDirectory;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<div>absPath:"+ path +"</div>");
+        EXEHelper a = new EXEHelper();
+        a.path = path + @"ExeFile\test\main.exe";
+        a.arguments = "hellow";
+        Response.Write("<div>absPath:" + a.OperationReturn() + "</div>");
+    }
+}
