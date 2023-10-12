8b90faa85124b36e46276db820b9c95d42eb72ae
雨禾 赵 <109513335+03xiaoyuhe@users.noreply.github.com>
2023-10-12 20:49:01 +0800

    新建了数据库连接测试页面
    new:
    DBHelperTest1011.aspx
    DBHelperTest1011.aspx.cs

---
+++ web/DBHelperTest1011.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class DBHelperTest : System.Web.UI.Page
+{
+    public DataTable dt = new DataTable();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //创建一个临时数据库
+            DataSet ds = new DataSet();
+            //创建一个临时表
+            dt = new DataTable();
+        }
+    }
+}
