7678ff82a51a6f06fec0c17763c55683fa3ae841
雨禾 赵 <109513335+03xiaoyuhe@users.noreply.github.com>
2023-11-30 16:23:01 +0800

    zyh 16:22

--- web/ASCX/RoughImformation.ascx.cs
+++ web/ASCX/RoughImformation.ascx.cs
@@ -12,25 +12,25 @@ public partial class ASCX_RoughImformation : System.Web.UI.UserControl
     public DataTable dt = new DataTable();
     protected void Page_Load(object sender, EventArgs e)
     {
+        //创建一个临时数据库
+        DataSet ds;
+        ds = DBHelper.Query("select *from UserInfo;");
+
+        //创建一个临时表
+        dt = ds.Tables[0];
+        string[] strColumns = null;
+        if (dt.Columns.Count > 0)
+        {
+            int columnNum = 0;
+            columnNum = dt.Columns.Count;
+            strColumns = new string[columnNum];
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                strColumns[i] = dt.Columns[i].ColumnName;
+            }
+        }
         if (!IsPostBack)
         {
-            //创建一个临时数据库
-            DataSet ds;
-            ds = DBHelper.Query("select *from UserInfo;");
-
-            //创建一个临时表
-            dt = ds.Tables[0];
-            string[] strColumns = null;
-            if (dt.Columns.Count > 0)
-            {
-                int columnNum = 0;
-                columnNum = dt.Columns.Count;
-                strColumns = new string[columnNum];
-                for (int i = 0; i < dt.Columns.Count; i++)
-                {
-                    strColumns[i] = dt.Columns[i].ColumnName;
-                }
-            }
         }
     }
 }
