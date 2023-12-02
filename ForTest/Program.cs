5cb78c213984d4fa599edbdf2dec6675a40e38ef
雨禾 赵 <109513335+03xiaoyuhe@users.noreply.github.com>
2023-12-02 19:20:30 +0800

    zyh 19:19 write for tesr DAL
    编写了用于测试DAL层的测试库test

---
+++ ForTest/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ForTest;
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+#region UserSighin
+/// UserLoginOn
+Console.WriteLine("\n\n----For UserSighin Test----");
+ForUserSigin.ForAuthenticateUser();
+
+#endregion
+
+
+
+#region UserLoginOn
+/// UserLoginOn
+Console.WriteLine("\n\n----For UserLoginOn Test----");
+ForUserLoginOn.ForCheckIfAccountExists();
+ForUserLoginOn.ForRegisterUser();
+
+#endregion
+
+
