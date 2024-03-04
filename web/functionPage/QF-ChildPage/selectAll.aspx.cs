99275766b82271c829a16cda8f3d31f1ed31bfc5
赵雨禾 <2297622003@qq.com>
2024-03-04 22:28:28 +0800

    故障测试 0304

--- web/functionPage/QF-ChildPage/selectAll.aspx.cs
+++ web/functionPage/QF-ChildPage/selectAll.aspx.cs
@@ -6,28 +6,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-public partial class functionPage_QF_ChildPage_selectAll : System.Web.UI.Page
+public partial class test : System.Web.UI.Page
 {
-    static public DataTable dt = new DataTable();
     protected void Page_Load(object sender, EventArgs e)
     {
-        //创建一个临时数据库
-        DataSet ds;
-        ds = DBHelper.Query("select *from UserLogin;");
 
-        //创建一个临时表
-        dt = ds.Tables[0];
-        string[] strColumns = null;
-        if (dt.Columns.Count > 0)
+    }
+
+    void LoadTable()
+    {
+        DataSet dataSet = ProjectCompletion.ProjectVision();
+        DataTable dataTable;
+        try
         {
-            int columnNum = 0;
-            columnNum = dt.Columns.Count;
-            strColumns = new string[columnNum];
-            for (int i = 0; i < dt.Columns.Count; i++)
-            {
-                strColumns[i] = dt.Columns[i].ColumnName;
-            }
+            dataTable = dataSet.Tables[0];
         }
+        catch
+        {
+            dataTable = null;
+        }
+        List<string> list = new List<string>();
+        list.Add("proposal_number");
+        list.Add("project_title");
+        list.Add("project_description");
+        list.Add("team_id");
+        list.Add("achievement_form");
+        list.Add("achievement_brief");
+
+        Dictionary<string, string> map = new Dictionary<string, string>();
+        map.Add("proposal_number", "立项编号");
+        map.Add("project_title", "项目主题");
+        map.Add("project_description", "项目描述");
+        map.Add("team_id", "小队id");
+        map.Add("achievement_form", "成果形式");
+        map.Add("achievement_brief", "成果简述");
+
+        TableAttribute tableAttribute = new TableAttribute(
+            "project_id",
+            "项目信息",
+            map,
+            list
+            );
+        ASCX_Table_Table NewLine = (ASCX_Table_Table)LoadControl("~/ASCX/Table/Table.ascx");
+        NewLine.TableBase = tableAttribute;
+        NewLine.DataCollection = dataTable;
+        NewLine.Height = 400;
+        PlaceHolder1.Controls.Add(NewLine);
+    }
+
+    protected void PlaceHolder1_Load(object sender, EventArgs e)
+    {
+        LoadTable();
     }
 }
--- web/functionPage/QueryForm.aspx
+++ web/functionPage/QueryForm.aspx
@@ -454,10 +454,9 @@
             </div>
         </div>
 
-        <script type="text/javascript">
-</script>
+        <script type="text/javascript"></script>
         <div class="bg-body-tertiary border rounded-3" style="float: right; position: relative; overflow: hidden; width: 100%; height: 570px; margin-right: 30px; margin-left">
-            <iframe id="test" src="../test.aspx" style="width: 100%; height: 100%"></iframe>
+            <%--<iframe id="test" src="./QF-ChildPage/selectAll.aspx" style="width: 100%; height: 100%"></iframe>--%>
         </div>
     </main>
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
