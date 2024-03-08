956ebe9947256726f33f58682bb63094d5e491d1
xxxlzm <117054278+xxxlzm@users.noreply.github.com>
2024-03-08 22:36:18 +0800

    3/8  写了关于删除与查找的通法

--- DAL/DAL/TableSelect.cs
+++ DAL/DAL/TableSelect.cs
@@ -9,32 +9,33 @@ using System.Security.Cryptography;
 
 namespace DAL
 {
-
-    public class TableDetele
+    public class TableSelect
     {
         /// <summary>
-        /// 对所有的表都适合的删除操作
-        /// 其中函数需要三个标量
+        /// 对所有的表都适合的查询单行操作
+        /// 其中函数需要三个变量
         /// 分别为 “表名” “字段名” “字段名对应的值”
         /// </summary>
         /// <param name="TableName">表名</param>
         /// <param name="CellId">字段名</param>
         /// <param name="Id">字段名对应的值</param>
         /// <returns></returns>
-        static public bool Delete(string TableName,string CellId,string Id)
+        static public bool Select(string TableName, string CellId, string Id)
         {
-            string query = "DELETE FROM @TableName WHERE @CellId=@Id;";
-            SqlParameter [] parameters =
+            string query = "SELECT *FROM @TableName WHERE @CellId=@Id;";
+
+            SqlParameter[] parameters =
              {
                 new SqlParameter("@TableName",SqlDbType.Int) {Value = TableName},
                 new SqlParameter("@CellId",SqlDbType.NVarChar) {Value = CellId},
                 new SqlParameter("@Id",SqlDbType.NVarChar) {Value=Id},
             };
+
             object rowsAffected = DBHelper.GetSingle(query, parameters);
 
             int count = Convert.ToInt32(rowsAffected);
 
-            return (count>0);
+            return (count > 0);
         }
     }
 }
