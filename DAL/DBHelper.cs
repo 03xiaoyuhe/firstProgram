bc918c593357f4a9220bfd92c738297e6687420f
03xiaoyuhe <2297622003@qq.com>
2024-03-09 22:19:26 +0800

    同步了历史存储更改，修改了文件夹结构，清楚了多余的bin文件夹

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DAL
+{
+    internal class DBHelper
+    {
+        /// <summary>
+        /// 获取通过账号名密码连接数据库的字符串
+        /// </summary>
+        /// <returns>连接字符串</returns>
+        public static string GetSqlConnectionString()
+        {
+            //string appSettings = ConfigurationManager.AppSettings["SQpwdLoad"];
+            //return appSettings.ToString();
+
+            return ConfigurationManager.ConnectionStrings["SQpwdLoad"].ConnectionString;
+        }
+
+        /// <summary>
+        /// 获取windows登录连接字符串
+        /// </summary>
+        /// <returns>连接字符串</returns>
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        }
+
+        /// <summary>
+        /// 适合增删改操作，返回影响条数
+        /// </summary>
+        /// <param name="sql">增删改语句</param>
+        /// <param name="parameters">字段</param>
+        /// <returns>影响条数</returns>
+        /// <exception cref="Exception"></exception>
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(GetSqlConnectionString()))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    try
+                    {
+                        conn.Open();
+                        comm.CommandText = sql;
+                        comm.Parameters.AddRange(parameters);
+                        return comm.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    finally
+                    {
+                        if (conn != null && conn.State != ConnectionState.Closed)
+                            conn.Close();
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 查询操作，返回查询结果中的第一行第一列的值
+        /// </summary>
+        /// <param name="sql">增删改语句</param>
+        /// <param name="parameters">字段</param>
+        /// <returns>查询结果中的第一行第一列的值</returns>
+        /// <exception cref="Exception"></exception>
+        public static object ExecuteScalar(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(GetSqlConnectionString()))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    try
+                    {
+                        conn.Open();
+                        comm.CommandText = sql;
+                        comm.Parameters.AddRange(parameters);
+                        return comm.ExecuteScalar();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    finally
+                    {
+                        if (conn != null && conn.State != ConnectionState.Closed)
+                            conn.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adapter调整，查询操作，返回DataTable
+        /// </summary>
+        /// <param name="sql">增删改语句</param>
+        /// <param name="parameters">字段</param>
+        /// <returns>DataTable</returns>
+        public static DataTable ExecuteDataTable(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sql, GetSqlConnectionString()))
+            {
+                DataTable dt = new DataTable();
+                adapter.SelectCommand.Parameters.AddRange(parameters);
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+
+        public static SqlDataReader ExecuteReader(string sqlText, params SqlParameter[] parameters)
+        {
+            //SqlDataReader要求，它读取数据的时候有，它独占它的SqlConnection对象，而且SqlConnection必须是Open状态
+            SqlConnection conn = new SqlConnection(GetSqlConnectionString());//不要释放连接，因为后面还需要连接打开状态
+            SqlCommand cmd = conn.CreateCommand();
+            conn.Open();
+            cmd.CommandText = sqlText;
+            cmd.Parameters.AddRange(parameters);
+            //CommandBehavior.CloseConnection当SqlDataReader释放的时候，顺便把SqlConnection对象也释放掉
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        /// <summary>
+        /// 执行查询语句，返回DataSet
+        /// </summary>
+        /// <param name="SQLString">查询语句</param>
+        /// <returns>DataSet</returns>
+        public static DataSet Query(string SQLString)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+                {
+                    DataSet ds = new DataSet();
+                    try
+                    {
+                        connection.Open();
+                        SqlDataAdapter command = new SqlDataAdapter(SQLString, connection);
+                        command.Fill(ds, "ds");
+                    }
+                    catch (System.Data.SqlClient.SqlException ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    return ds;
+                }
+            }
+            catch (Exception err)
+            {
+
+                return null;
+            }
+        }
+    }
+}
---
+++ EXE/EXEHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EXE
+{
+    public class EXEHelper
+    {
+        /// <summary>
+        /// EXE文件的绝对路径
+        /// </summary>
+        public string path;
+        public string Arguments;
+        /// <summary>
+        /// 运行程序并返回运行结果
+        /// </summary>
+        /// <returns>EXE文件返回(字符串)</returns>
+        public string OperationReturn()
+        {
+            Process exe = new Process();
+            exe.StartInfo.FileName = path;
+            exe.StartInfo.Arguments = " " + Arguments;
+            exe.StartInfo.UseShellExecute = false;
+            exe.StartInfo.RedirectStandardOutput = true;
+            exe.StartInfo.RedirectStandardInput = true;
+            exe.StartInfo.CreateNoWindow = true;
+            exe.Start();
+            exe.WaitForExit();
+            string data = exe.StandardOutput.ReadToEnd();
+            return data;
+        }
+    }
+}
