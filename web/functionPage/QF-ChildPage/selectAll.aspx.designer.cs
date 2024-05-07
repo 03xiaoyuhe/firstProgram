05c4c854555f736deac173b28bdd64c1bf0157fa
03xiaoyuhe <2297622003@qq.com>
2024-05-07 22:17:44 +0800

    样式美化

--- web/functionPage/QF-ChildPage/selectAll.aspx.designer.cs
+++ web/functionPage/QF-ChildPage/selectAll.aspx.designer.cs
@@ -24,31 +24,22 @@ namespace WebForm.functionPage.QF_ChildPage
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Button2 控件。
+        /// Button3 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button Button3;
 
         /// <summary>
-        /// ByProgremNameButton 控件。
+        /// Button4 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ByProgremNameButton;
-
-        /// <summary>
-        /// ByProgremNoButton 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ByProgremNoButton;
+        protected global::System.Web.UI.WebControls.Button Button4;
 
         /// <summary>
         /// ScriptManager1 控件。
